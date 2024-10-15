d5e74ea9650d59cd56483b1521325a4b5ee577b7
datarampalli <131389836+datarampalli@users.noreply.github.com>
2024-10-14 21:02:13 -0500

    Update and rename postgres_db_user_creation.sql to Dockerfile

--- postgres_db_user_creation.sql
+++
@@ -1,8 +0,0 @@
-# 8. Create the user 'mpuser' with password 'mppassword'
-CREATE USER mpuser WITH PASSWORD 'mppassword';
-
-# 9. Create the database 'mp_db' with 'mpuser' as the owner
-CREATE DATABASE mp_db OWNER mpuser;
-
-# 10. Grant all privileges on 'mp_db' to 'mpuser'
-GRANT ALL PRIVILEGES ON DATABASE mp_db TO mpuser;
